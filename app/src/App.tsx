f9b3b7add4268ffc3c0d87d3db04d04b57523948
lizblackledge <lizblackledge@users.noreply.github.com>
2023-10-07 19:58:21 -0500

    Post request to get access token

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    const authParams = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&client_secret=${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`,
+    };
+    try {
+      fetch('https://accounts.spotify.com/api/token', authParams)
+      .then(result => result.json())
+      .then(data => setAccessToken(data.access_token))
+    } catch(e) {
+      console.log('error: ', e)
+    }
+  });
+
   return (
     <div className="App">
-      <h1>hejsan!</h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <h1>hejsan allihop!</h1>
     </div>
   );
 }
